a9de5b5fb40661294c71ea6157f1cacf80557a7c
vileda <vileda@vileda.cc>
2016-02-20 14:27:08 +0100

    sleep 1 second before posting

--- src/test/java/eventstore/boundary/StompBridgeTest.java
+++ src/test/java/eventstore/boundary/StompBridgeTest.java
@@ -87,7 +87,7 @@ public class StompBridgeTest {
 								});
 						vertx.executeBlocking(fut -> {
 							try {
-								Thread.sleep(2000);
+								Thread.sleep(1000);
 							} catch (InterruptedException e) {
 								e.printStackTrace();
 							}
